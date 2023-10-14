e1babb95bc272dbdfd46b8b1bc23c8e9c0c2e92d
MobKill3r2006 <supermanwouter@gmail.com>
2023-10-14 20:34:17 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ $(function () {
     $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500, "linear");
   });
 });
+
+(function () {
+  var target = $("#section02");
+  $("#wrapper").scroll(function () {
+    target.prop("scrollTop", this.scrollTop).prop("scrollLeft", this.scrollLeft);
+  });
+})();
