e9f01d6fc59277a46b1a97e0d339ccb365decd5c
MobKill3r2006 <supermanwouter@gmail.com>
2023-10-17 21:53:23 +0200

    Major update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 $(function () {
   $('a[href*="#"]').on("click", function (e) {
     e.preventDefault();
-    $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500, "linear");
+    if ($(this).attr("href") == "#section01" || $(this).attr("href") == "#section01-desk") $("html, body").animate({ scrollTop: 0 }, 500, "linear");
+    else if ($(this).attr("href") == "#section02" || $(this).attr("href") == "#section02-desk")
+      $("html, body").animate({ scrollTop: $($(this).attr("href")).height() }, 500, "linear");
+    else if ($(this).attr("href") == "#section03")
+      $("html, body").animate({ scrollTop: $("#section02").height() + $($(this).attr("href")).height() }, 500, "linear");
+    else if ($(this).attr("href") == "#section03-desk")
+      $("html, body").animate({ scrollTop: $("#section02-desk").height() + $($(this).attr("href")).height() }, 500, "linear");
   });
 });
-
-(function () {
-  var target = $("#section02");
-  $("#wrapper").scroll(function () {
-    target.prop("scrollTop", this.scrollTop).prop("scrollLeft", this.scrollLeft);
-  });
-})();
